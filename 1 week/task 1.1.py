424fd367dc8b7c3d0cb67c326a137f4609e9ae8b
Bahdan321 <130000369+Bahdan321@users.noreply.github.com>
2025-04-17 12:55:57 +0200

    feat: add solution to task 1.1

---
+++ 1 week/task 1.1.py
@@ -0,0 +1,15 @@
+x = 26
+
+
+first = x
+second = 2 * x
+third = 2 * x - 10
+
+
+print(f"Первый получил: {first} рублей")
+print(f"Второй получил: {second} рублей")
+print(f"Третий получил: {third} рублей")
+
+
+total = first + second + third
+print(f"Проверка: {first} + {second} + {third} = {total} рублей")
